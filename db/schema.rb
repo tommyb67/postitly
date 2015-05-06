e11995a996fdf0436c6bec46ad448caecb96173e
Tom Brennan <tommybrennan@gmail.com>
2015-05-06 09:23:54 -0400

    created the user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506131119) do
+ActiveRecord::Schema.define(version: 20150506132029) do
 
   create_table "posts", force: true do |t|
     t.string   "url"
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
