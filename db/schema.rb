7ac1d2fcb26d13cd59c87f2b265e2a89c5cf2724
Tom Brennan <tommybrennan@gmail.com>
2015-05-06 11:05:19 -0400

    added a foreign key to the posts model and established the relationships between posts and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506132029) do
+ActiveRecord::Schema.define(version: 20150506150152) do
 
   create_table "posts", force: true do |t|
     t.string   "url"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150506132029) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
