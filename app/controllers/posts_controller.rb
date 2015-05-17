c53b6f888e2a5bcb8d294a947cd1278a638cb35a
Tom Brennan <tommybrennan@gmail.com>
2015-05-17 17:07:36 -0400

    added method stubs to the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
