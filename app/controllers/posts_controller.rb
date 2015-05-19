a50e5b94145a770448f952868a46be7782eb842f
Tom Brennan <tommybrennan@gmail.com>
2015-05-19 12:46:12 -0400

    making progress on the posts controller and model backed form submissions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,15 +9,31 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(params[:post])
+
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit!
   end
 
 end
