336c75dd798839234667fb6006a96b51774d6f50
Tom Brennan <tommybrennan@gmail.com>
2015-05-23 23:03:33 -0400

    updated the post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
 
-  #validates :title, presence: true
-  #validates :url, presence: true
+  validates :title, presence: true
+  validates :url, presence: true
 end
